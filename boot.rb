7e499f0299384c99c88e69b4e175c51dbda3cc54
Paul Annesley <paul@annesley.cc>
2011-09-28 03:45:07 +1000

    Lexer, Parser and Loader instantiated in boot.rb

--- boot.rb
+++ boot.rb
@@ -1,8 +1,14 @@
 require_relative "vm"
 require_relative "vm/core"
 require_relative "vm/kernel"
+require_relative "vm/lexer"
+require_relative "vm/parser"
+require_relative "vm/loader"
 
 vm = VM::Core.new
 kernel = VM::Kernel.new(vm)
+lexer = VM::Lexer.new
+parser = VM::Parser.new
+loader = VM::Loader.new(vm)
 
 
