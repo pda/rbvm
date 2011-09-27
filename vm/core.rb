1e99cb111bc69e0e4c5b8ff714563cc4863d435c
Paul Annesley <paul@annesley.cc>
2011-09-28 02:50:24 +1000

    Removed unused :c and :d registers.

--- vm/core.rb
+++ vm/core.rb
@@ -6,7 +6,7 @@ class VM::Core
 
   def initialize
     @mem = VM::Memory.new(0x20)
-    @reg = Struct.new(:sp, :a, :b, :c, :d).new.tap do |reg|
+    @reg = Struct.new(:sp, :a, :b).new.tap do |reg|
       reg.sp = @mem.size - 1
     end
     @cis = VM::Cis.new(self)
